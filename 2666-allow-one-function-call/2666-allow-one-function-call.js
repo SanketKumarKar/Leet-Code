95f9df95ade7aa1a3d085b627c81b4154819d2a0
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2025-12-30 20:25:18 +0530

    Time: 46 ms (56.35%), Space: 53.9 MB (64.36%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -1,21 +1,2 @@
-/**
- * @param {Function} fn
- * @return {Function}
- */
-var once = function(fn) {
-    let x = 1;
-    return function(...args){
-        if (x==1){
-            x=0;
-           return fn(...args);
-        }else {return;}
-    }
-};
-
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+var once=function(n){let r=1;return function(...t){return 1==r?(r=0,n(...t)):void 0}};
+//minified;
