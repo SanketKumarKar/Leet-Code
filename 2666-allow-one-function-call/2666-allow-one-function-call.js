ef7fa5e813040ad0e7907956f7b66b3d8bd1450e
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2025-12-30 20:25:57 +0530

    Time: 52 ms (22.49%), Space: 52.9 MB (96.14%) - LeetHub

--- 2666-allow-one-function-call/2666-allow-one-function-call.js
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -1,2 +1,2 @@
-var once=function(n){let r=1;return function(...t){return 1==r?(r=0,n(...t)):void 0}};
+var once = function (n) { let r = 1; return function (...t) { return 1 == r ? (r = 0, n(...t)) : void 0 } };
 //minified;
