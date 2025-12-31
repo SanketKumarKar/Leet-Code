180ae2c1f5d371970b973fc300cb2278bff587de
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2025-12-31 16:48:22 +0530

    Time: 58 ms (42.76%), Space: 54 MB (89.17%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,9 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = function(promise1, promise2) {
-    return Promise.all([promise1, promise2])
-        .then(([a, b]) => a + b);
-};
+    return Promise.all([promise1, promise2])//resolve all promises.
+        .then(([a, b]) => a + b);//values given from the promises are added.
+};// can be done using async and await tooo.
 
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
