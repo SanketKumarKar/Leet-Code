dee55aac8d1ec38c7ad53110d5973fef9ad56e27
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2025-12-31 16:46:34 +0530

    Time: 56 ms (53.17%), Space: 54.4 MB (74.82%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = function(promise1, promise2) {
+    return Promise.all([promise1, promise2])
+        .then(([a, b]) => a + b);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
