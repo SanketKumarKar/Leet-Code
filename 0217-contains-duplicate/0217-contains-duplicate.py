bec2b0c960bb6a5e4aa4b9c256f8f2197c397fe4
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2026-06-08 12:46:05 +0530

    Add comments to containsDuplicate method
    Added comments to clarify the logic for checking duplicates.

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # set removes duplicates
         test = set(nums)
+        # simple check the lenght now of test and list
         if len(nums) == len(test):
             return False
         else:
