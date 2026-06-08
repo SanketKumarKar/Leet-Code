9a71bfa870b5619294e7f268db570f514414066b
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2026-06-08 12:10:52 +0530

    Time: 5 ms (17.75%), Space: 20.4 MB (51.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        
+        # observation ery day when stock rises from 
+        # the previous day the difference is the profit        
         for i in range(1,len(prices)):
             
             if prices[i]>prices[i-1]:
