5aefc3b2fb418906872f1e7ad55017677273f013
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2026-06-11 22:37:58 +0530

    Time: 24 ms (83.76%), Space: 30.8 MB (82.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,18 +1,16 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
-        n = len(nums)
-
-        for i in range(n):
-            while (
-                1 <= nums[i] <= n
-                and nums[i] != nums[nums[i] - 1]
-            ):
-                correct = nums[i] - 1
-                nums[i], nums[correct] = nums[correct], nums[i]
-
-        for i in range(n):
-            if nums[i] != i + 1:
-                return i + 1
-
-        return n + 1   
+        nums.sort()
+        
+        result = 1
+        
+        for num in nums:
+            
+            if num == result:
+                
+                result+=1
+                
+            elif num > result:
+                break
+        return result
