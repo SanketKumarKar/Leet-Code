cb3b3f2e4a3a106dcc7f14d4db69ed5d62b963b3
Sanket Kumar Kar <sanketkumarkar@gmail.com>
2026-06-22 23:57:30 +0530

    Time: 8 ms (41.77%), Space: 23.1 MB (39.65%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,16 +1,16 @@
 class Solution:
     def merge(self, arr):
-
+        # sort to make life ez
         arr.sort()
 
         res = [arr[0]]
 
         for start, end in arr[1:]:
-
+            #compare prev range last elt to next range start elt
             if res[-1][1] >= start:
-
+                #update the range
                 res[-1][1] = max(end, res[-1][1])
-
+            #if not overlapping append result
             else:
                 res.append([start, end])
 
